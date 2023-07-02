baaa6a23ee55ad4fae6373e4ea946b8d32712e32
Mathias Picker <mattis123@live.no>
2023-07-02 17:30:38 +0200

    add link to p5js on landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,15 @@
 
 <div class="h-full w-full flex items-center justify-center">
 	<div class="flex flex-col gap-6 z-10">
-		<div class="flex items-end gap-6 text-8xl">svelte<P5Logo height={65} /></div>
-		<p>a safe, fast and declarative Svelte component library for the p5.js library</p>
+		<div class="flex items-end gap-6 text-8xl">svelte<P5Logo height={68} /></div>
+		<p>
+			a safe, fast and declarative Svelte component library for <a
+				href="https://p5js.org/"
+				target="_blank"
+				class="underline"
+				rel="noopener noreferrer">p5.js</a
+			>
+		</p>
 	</div>
 	<div class="absolute top-0 left-0 z-0">
 		<SimulateSoftBody />
